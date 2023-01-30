d43bfbfd6331cc06488b17703b632bd3fcf58c2c
Alex <alexander.ogier@gmail.com>
2023-01-31 00:31:33 +1300

    wip, logic sound but permutations are highly unoptimal

--- 60.js
+++ 60.js
@@ -23,37 +23,100 @@ function isPrime(n) {
     return true;
 }
 
+var LEN = 5;
 var primes = [];
-var primeMap = {};
+var startsAndEnds = {};
 
-for(var i = 2; i < 100000; i++) {
+var checkedAThing = false;
+
+function getCombinations(array) {
+    var result = [];
+
+    function fork(i, t) {
+        if (t.length === 4) {
+            result.push(t);
+            return;
+        }
+        if (i === array.length) {
+            return;
+        }
+        fork(i + 1, t.concat([array[i]]));
+        fork(i + 1, t);
+    }
+
+    fork(0, []);
+    return result;
+}
+
+for(var i = 2; true; i++) {
     if (isPrime(i)) {
         primes.push(i);
-        primeMap[i] = true;
+        //primeMap[i] = true;
+
+        for(var qi = 0; qi < primes.length-1; qi++) {
+            var p = i;
+            var q = primes[qi];
+    
+            var pq = parseInt(p+''+q);
+            var qp = parseInt(q+''+p);
+            //if(primeMap[pq] && primeMap[qp]) {
+            if(isPrime(pq) && isPrime(qp)) {
+                startsAndEnds[p] = startsAndEnds[p] || {};
+                startsAndEnds[p][q] = q;
+    
+                startsAndEnds[q] = startsAndEnds[q] || {};
+                startsAndEnds[q][p] = p;
+
+                var vals = Object.values(startsAndEnds[p]);
+                if (vals.length < LEN-1) {
+                    continue;
+                }
+
+                // permute vals and take 4
+                var permutationsOfVals = getCombinations(vals.map(n => n));
+
+                console.log(permutationsOfVals);
+
+                permutationsOfVals.forEach(pov => {
+                    var mapCount = pov.reduce((a, c) => {
+                        a[c] = 1;
+                        return a;
+                    }, {});
+                    mapCount[p] = 1;
+
+                    pov.forEach(pp => {
+                        mapCount[pp]++;
+                        var sande = Object.values(startsAndEnds[pp]);
+                        sande.forEach(sae => {
+                            if (mapCount[sae]) {
+                                mapCount[sae]++;
+                            }
+                        });
+                    })
+
+                    var setWithEnough = Object.values(mapCount).filter(mc => mc >= LEN).length == LEN;
+                    if (setWithEnough) {
+                        var primeSet = Object.entries(mapCount).filter(e => e[1] >= LEN).map(e => parseInt(e[0]));
+                        primeSet.forEach(vv => {
+                            console.log(vv, Object.values(startsAndEnds[vv]).join(','));
+                        })
+                        console.log('result', primeSet, primeSet.reduce((a, c) => a + c, 0));
+                        checkedAThing = true;
+                    }
+                });
+
+                if (checkedAThing) {
+                    return;
+                }
+            }
+        }
     }
 }
 
-var startsAndEnds = {};
+// for(var pi = 0; pi < primes.length; pi++) {
+    
+// }
 
-var stringsByCount = {};
+//console.log(Object.values(startsAndEnds).filter(l => l.length >= 5).length);
 
-primes.forEach(p => {
-    primes.forEach(q => {
-        if (p != q) {
-            //ends
-            var pq = p+''+q;
-            var qp = q+''+p;
-            if(primeMap[pq] && primeMap[qp]) {
-                startsAndEnds[p] = startsAndEnds[p] || { elements: [p], str: ''};
-                startsAndEnds[p].elements.push(q);
-                startsAndEnds[p].str = startsAndEnds[p].elements.sort().join(',');
-
-                startsAndEnds[q] = startsAndEnds[q] || { elements: [q], str: '' };
-                startsAndEnds[q].elements.push(p);
-                startsAndEnds[q].str = startsAndEnds[q].elements.sort().join(',');
-            }
-        }
-    })
-})
-
-console.log(primes);
+//console.log(startsAndEnds);
